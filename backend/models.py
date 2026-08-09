b20771cfdeeccb08285396493cbb08fd243e076e
Abhishek Katwal <abhishek20.katwal@gmail.com>
2026-08-10 02:44:55 +0530

    fix database import in models.py

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 
-from database import Base
+from .database import Base
 
 
 # =========================================================
